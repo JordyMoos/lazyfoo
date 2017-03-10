e70367182651463fe79d68d9fd884564a3181574
Jordy Moos <jordy@pbwebmedia.com>
2017-03-10 11:04:42 +0100

    Chapter 09

--- main.cpp
+++ main.cpp
@@ -62,40 +62,21 @@ int main(int argc, char* args[])
             }
         }
 
-        // Clear screen
-        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        SDL_RenderClear(gRenderer);
+        SDL_Rect topLeftViewport;
+        topLeftViewport.x = 0;
+        topLeftViewport.y = 0;
+        topLeftViewport.w = SCREEN_WIDTH / 2;
+        topLeftViewport.h = SCREEN_HEIGHT / 2;
+        SDL_RenderSetViewport(gRenderer, &topLeftViewport);
+        SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
-        // Render red quad
-        SDL_Rect fillRect = {
-            SCREEN_WIDTH / 4,
-            SCREEN_HEIGHT / 4,
-            SCREEN_WIDTH / 2,
-            SCREEN_HEIGHT / 2
-        };
-        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-        SDL_RenderFillRect(gRenderer, &fillRect);
-
-        // Render green quad
-        SDL_Rect outlineRect = {
-            SCREEN_WIDTH / 6,
-            SCREEN_HEIGHT / 6,
-            SCREEN_WIDTH * 2 / 3,
-            SCREEN_HEIGHT * 2 / 3
-        };
-        SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
-        SDL_RenderDrawRect(gRenderer, &outlineRect);
-
-        // Draw blue horizontal line
-        SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
-        SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
-
-        // Draw vertical line of yellow dots
-        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
-        for (int i = 0; i < SCREEN_HEIGHT; i += 4)
-        {
-            SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
-        }
+        SDL_Rect topRightViewport;
+        topRightViewport.x = SCREEN_WIDTH / 2;
+        topRightViewport.y = 0;
+        topRightViewport.w = SCREEN_WIDTH / 2;
+        topRightViewport.h = SCREEN_HEIGHT / 2;
+        SDL_RenderSetViewport(gRenderer, &topRightViewport);
+        SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
         SDL_RenderPresent(gRenderer);
     }
@@ -144,6 +125,13 @@ bool init()
 
 bool loadMedia()
 {
+    gTexture = loadTexture("assets/viewport.png");
+    if (gTexture == nullptr)
+    {
+        sprintf("Failed to load texture %s\n", "assets/viewport.png");
+        return false;
+    }
+
     return true;
 }
 
